5739cb1406a70fd9db0636d2379a6e60931a4c4b
Qandra Si <qandra.si@gmail.com>
2024-11-26 02:37:47 +0300

    актуализировал скрипт обновления до v1.1.2

--- database/100.update-v1.1.2.sql
+++ database/100.update-v1.1.2.sql
@@ -8,5 +8,15 @@ UPDATE qz.zkillmails
 SET zkm_published = TRUE
 WHERE zkm_id IN (SELECT p_killmail_id FROM qz.published);
 
+CREATE INDEX idx_zkm_published
+    ON qz.zkillmails USING btree
+    (zkm_published ASC NULLS LAST)
+TABLESPACE pg_default;
+
+CREATE INDEX idx_zkm_need_refresh
+    ON qz.zkillmails USING btree
+    (zkm_need_refresh ASC NULLS LAST)
+TABLESPACE pg_default;
+
 DROP INDEX IF EXISTS idx_pk_p;
 DROP TABLE IF EXISTS qz.published;
