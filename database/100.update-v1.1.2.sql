8e94ed0b26926f45c5c5845531f9b938e1a0cd26
Qandra Si <qandra.si@gmail.com>
2024-11-26 02:29:40 +0300

    скрипт обновления до v1.1.2 теперь и удаляет ненужную таблицу

--- database/100.update-v1.1.2.sql
+++ database/100.update-v1.1.2.sql
@@ -7,3 +7,6 @@ ALTER TABLE qz.zkillmails ADD zkm_need_refresh BOOLEAN DEFAULT FALSE NOT NULL;
 UPDATE qz.zkillmails
 SET zkm_published = TRUE
 WHERE zkm_id IN (SELECT p_killmail_id FROM qz.published);
+
+DROP INDEX IF EXISTS idx_pk_p;
+DROP TABLE IF EXISTS qz.published;
