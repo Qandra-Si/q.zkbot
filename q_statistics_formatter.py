578c30f9f494d90d54a325d21d9c30d55a15723f
Qandra Si <qandra.si@gmail.com>
2024-12-27 18:46:32 +0300

    описал словами статистику

--- q_statistics_formatter.py
+++ q_statistics_formatter.py
@@ -21,5 +21,39 @@ class FormattedDiscordStatisticsMessage:
 
         self.paginator = discord.ext.commands.Paginator(prefix='', suffix='')
         self.paginator.add_line("**Статистика подъехала**")
-        for key, data in self.__stat.items():
-            self.paginator.add_line(str(key))
+
+        s = self.__stat.get('solo_loss')
+        if s:
+            cnt: int = s['cnt']
+            destroyed: int = s['fitted']
+            dropped: int = s['dropped']
+            self.paginator.add_line(
+                f"Мы слились в честном соло-pvp {cnt} раз, "
+                f"потеряв {destroyed} ISK ({dropped} ISK досталось врагу).")
+
+        s = self.__stat.get('gang_loss')
+        if s:
+            cnt: int = s['cnt']
+            destroyed: int = s['fitted']
+            dropped: int = s['dropped']
+            self.paginator.add_line(
+                f"Об ганги мы убились {cnt} раз, "
+                f"потеряв {destroyed} ISK ({dropped} ISK досталось врагу).")
+
+        s = self.__stat.get('solo_win')
+        if s:
+            cnt: int = s['cnt']
+            destroyed: int = s['fitted']
+            dropped: int = s['dropped']
+            self.paginator.add_line(
+                f"В честном соло-pvp мы победили {cnt} раз, "
+                f"уничтожив {destroyed} ISK ({dropped} ISK дропнулось).")
+
+        s = self.__stat.get('gang_win')
+        if s:
+            cnt: int = s['cnt']
+            destroyed: int = s['fitted']
+            dropped: int = s['dropped']
+            self.paginator.add_line(
+                f"Флотами было уничтожено {cnt} противников, "
+                f"на сумму {destroyed} ISK ({dropped} ISK дропнулось).")
